a7ed752142e4aaa3b06d5880f85377e9805ef669
OwaisNITK <owais.nitk@gmail.com>
2020-05-15 22:01:27 +0530

    Queries Updated

--- Analyis_Updates_2.0/item_analysis.sh
+++ Analyis_Updates_2.0/item_analysis.sh
@@ -10,16 +10,18 @@ FILE_NAME=REPORT_"$CURRENT_DATE"_"$CURRENT_TIME".txt
 echo "Generating Report . . ." > $ANALYSIS_REPORT_DIR/$FILE_NAME
 echo "" >> $ANALYSIS_REPORT_DIR/$FILE_NAME
 
-echo "-> Follwing is the most viewed item across the website" >> $ANALYSIS_REPORT_DIR/$FILE_NAME
-hive -e "QUERY" >> $ANALYSIS_REPORT_DIR/$FILE_NAME
+echo "-> Follwing is the most viewed item across the website considering all the records" >> $ANALYSIS_REPORT_DIR/$FILE_NAME
+echo "" >> $ANALYSIS_REPORT_DIR/$FILE_NAME
+echo "ITEM | VIEWS | RANK" >> $ANALYSIS_REPORT_DIR/$FILE_NAME
+echo "" >> $ANALYSIS_REPORT_DIR/$FILE_NAME
+hive -e "SELECT item,viewcount,rank from (SELECT *,rank() OVER (ORDER BY viewcount DESC) rank from (SELECT reverse(split(reverse(url),\"/\")[0]) item,count(url) as viewcount from clickstream.activity_partitioned WHERE action=\"view\" GROUP BY reverse(split(reverse(url),\"/\")[0])) b ) a where rank=1" >> $ANALYSIS_REPORT_DIR/$FILE_NAME
 echo "" >> $ANALYSIS_REPORT_DIR/$FILE_NAME
 
-echo "-> Following are the location wise analysis of the most viewed item" >> $ANALYSIS_REPORT_DIR/$FILE_NAME
-hive -e "QUERY" >> $ANALYSIS_REPORT_DIR/$FILE_NAME
+echo "-> Following are the location wise analysis of the most viewed item today" >> $ANALYSIS_REPORT_DIR/$FILE_NAME
 echo "" >> $ANALYSIS_REPORT_DIR/$FILE_NAME
-
-echo "-> Following are the location and date wise analysis of the most sold item" >> $ANALYSIS_REPORT_DIR/$FILE_NAME
-hive -e "QUERY" >> $ANALYSIS_REPORT_DIR/$FILE_NAME
+echo "LOCATION | ITEM | VIEWS | RANK" >> $ANALYSIS_REPORT_DIR/$FILE_NAME
+echo "" >> $ANALYSIS_REPORT_DIR/$FILE_NAME
+hive -e "SELECT location,item,viewcount,rank from (SELECT *,rank() OVER (PARTITION BY location ORDER BY viewcount DESC) rank from (SELECT location,reverse(split(reverse(url),\"/\")[0]) item,count(url) as viewcount from clickstream.activity_partitioned WHERE logDate=CURRENT_DATE() AND action=\"view\" GROUP BY location,reverse(split(reverse(url),\"/\")[0])) b) a where rank=1" >> $ANALYSIS_REPORT_DIR/$FILE_NAME
 echo "" >> $ANALYSIS_REPORT_DIR/$FILE_NAME
 
 echo "Analysis Finished"
